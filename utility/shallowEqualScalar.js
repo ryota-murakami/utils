e8155ba22f733dcd8ba6aadf198eaf9f3f3e25c8
ryota-murakami <dojce1048@gmail.com>
2021-09-26 02:58:16 +0900

    add shallowEqual

---
+++ utility/shallowEqualScalar.js
@@ -0,0 +1,46 @@
+// from https://github.com/reduxjs/react-redux/blob/7a3e2fd11c9898e28700cad963757b523e215ab4/src/utils/shallowEqualScalar.js
+'use strict'
+
+exports.__esModule = true
+exports['default'] = shallowEqualScalar
+
+function shallowEqualScalar(objA, objB) {
+  if (objA === objB) {
+    return true
+  }
+
+  if (
+    typeof objA !== 'object' ||
+    objA === null ||
+    typeof objB !== 'object' ||
+    objB === null
+  ) {
+    return false
+  }
+
+  var keysA = Object.keys(objA)
+  var keysB = Object.keys(objB)
+
+  if (keysA.length !== keysB.length) {
+    return false
+  }
+
+  // Test for A's keys different from B.
+  var hasOwn = Object.prototype.hasOwnProperty
+  for (var i = 0; i < keysA.length; i++) {
+    if (!hasOwn.call(objB, keysA[i])) {
+      return false
+    }
+
+    var valA = objA[keysA[i]]
+    var valB = objB[keysA[i]]
+
+    if (valA !== valB || typeof valA === 'object' || typeof valB === 'object') {
+      return false
+    }
+  }
+
+  return true
+}
+
+module.exports = exports['default']

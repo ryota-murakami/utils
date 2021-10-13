1768b5935c6f7dc1aa14d7b0a6614dab6abc26cd
ryota-murakami <dojce1048@gmail.com>
2021-10-13 09:44:01 +0900

    up

--- utility/utility.d.ts
+++ utility/utility.d.ts
@@ -55,3 +55,23 @@ declare type Typify<T> = { [K in keyof T]: T[K] }
 declare type IndexSignature<O extends object> = {
   [P in keyof O]: O[P]
 }
+
+/**
+ * return True if T is `any`, otherwise return False
+ * taken from https://github.com/joonhocho/tsdef
+ *
+ * @internal
+ */
+export type IsAny<T, True, False = never> =
+  // test if we are going the left AND right path in the condition
+  true | false extends (T extends never ? true : false) ? True : False
+
+/**
+ * return True if T is `unknown`, otherwise return False
+ * taken from https://github.com/joonhocho/tsdef
+ *
+ * @internal
+ */
+export type IsUnknown<T, True, False = never> = unknown extends T
+  ? IsAny<T, False, True>
+  : False

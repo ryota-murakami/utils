6ac5711d89b10a4bafdd96c9bca5aaed4becaba0
ryota-murakami <dojce1048@gmail.com>
2021-10-14 19:53:06 +0900

    fix ts error

--- utility/typeHelpers.ts
+++ utility/typeHelpers.ts
@@ -1,8 +1,7 @@
 // from https://github.com/reduxjs/redux-toolkit/blob/4fbd29f0032f1ebb9e2e621ab48bbff5266e312c/packages/toolkit/src/query/tsHelpers.ts
-// eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/explicit-module-boundary-types
+
 export function assertCast<T>(v: any): asserts v is T {}
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function safeAssign<T extends Record<string, unknown>>(
   target: T,
   ...args: Array<Partial<NoInfer<T>>>

5b061b7a7aecb9039316c3f148f41a05f6c046ce
ryota-murakami <dojce1048@gmail.com>
2021-11-14 19:37:22 +0900

    update writeJson.js

--- utility/writeJson.js
+++ utility/writeJson.js
@@ -2,9 +2,18 @@ import fs from 'fs'
 import os from 'os'
 
 export function writeJson(fileName, object) {
+  if (Array.isArray(object)) {
+    const obj = { [fileName]: object }
+    fs.writeFileSync(
+      fileName + '.json',
+      JSON.stringify(obj, null, 2).replace(/\n/g, os.EOL) + os.EOL
+    )
+    return
+  }
+
   RegExp.prototype.toJSON = RegExp.prototype.toString
   fs.writeFileSync(
-    fileName,
+    fileName + '.json',
     JSON.stringify(object, null, 2).replace(/\n/g, os.EOL) + os.EOL
   )
 }

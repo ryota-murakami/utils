951d58d48086b34c3f8c6f7a04ddd07e686837ed
ryota-murakami <dojce1048@gmail.com>
2022-01-21 01:07:54 +0900

    uuu

--- packages/react/components/SuccessCard.jsx
+++ packages/react/components/SuccessCard.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import parse from 'style-to-object'
 
 const SuccessCard = () => (
   <div
     className="flex py-1 px-4 mb-2 font-sans text-sm font-normal tracking-normal leading-normal text-green-200 bg-gray-800 bg-none shadow-none"
     role="alert"
-    style={parse(
-      'transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms; border-radius: 10px;'
-    )}
+    style={{
+      background: '#fff',
+      margin: '130px 0 40px 0',
+      position: 'relative',
+      'box-shadow':
+        '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1)',
+    }}
   >
     <div
       className="flex py-2 px-0 mr-3 text-xl leading-8 text-green-600"
-      style={parse('opacity: 0.9;')}
+      style={{ opacity: 0.9 }}
     >
       <svg
         className="inline-block flex-shrink-0 w-4 h-4 leading-7 select-none"
@@ -19,7 +22,10 @@ const SuccessCard = () => (
         aria-hidden="true"
         viewBox="0 0 24 24"
         data-testid="CheckCircleRoundedIcon"
-        style="fill: currentcolor; transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;"
+        style={{
+          fill: 'currentcolor',
+          transition: 'fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+        }}
       >
         <path
           d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.29 16.29 5.7 12.7a.9959.9959 0 0 1 0-1.41c.39-.39 1.02-.39 1.41 0L10 14.17l6.88-6.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-7.59 7.59c-.38.39-1.02.39-1.41 0z"

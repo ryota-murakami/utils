633db3f5a84b9791ee989b816945261a7dec83df
ryota-murakami <dojce1048@gmail.com>
2022-01-27 01:28:04 +0900

    dir

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,6 +1,6 @@
 const parse = require('style-to-object')
 console.log(
   parse(
-'fill: currentcolor; transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;'
+    'fill: currentcolor; transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;'
   )
 )

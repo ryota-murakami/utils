6277c7e86611260a5af5229350c43a3e18594304
ryota-murakami <dojce1048@gmail.com>
2022-05-14 10:03:24 +0900

    up

--- src/layouts/Max80remCointainer.tsx
+++ src/layouts/Max80remCointainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Max80remCointainer: React.FC = ({ children }) => (
-  <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">{children}</div>
-)
+const Max80remCointainer: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">{children}</div>
 
 export default Max80remCointainer

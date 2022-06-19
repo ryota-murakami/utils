6cc160a011f047f17ce3330a17725037e914903c
ryota-murakami <dojce1048@gmail.com>
2022-06-20 05:51:06 +0900

    add arrayFillmap.test.ts

--- src/utility/__tests__/arrayFillmap.test.ts
+++ src/utility/__tests__/arrayFillmap.test.ts
@@ -1,1 +1,19 @@
 import { arrayFillmap } from '../ArrayFilllmap'
+
+test('run arrayFillmap', () => {
+  const res = arrayFillmap(10, (v, i) => {
+    return [i, v]
+  })
+  expect(res).toEqual([
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+    [5, 0],
+    [6, 0],
+    [7, 0],
+    [8, 0],
+    [9, 0],
+  ])
+})

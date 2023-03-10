61a704d9577c553534fb1a5da546e78ffd411ee7
ryota-murakami <dojce1048@gmail.com>
2023-03-10 11:30:22 +0900

    add suppressConsoleError

--- src/util/suppressConsoleError.js
+++ src/util/suppressConsoleError.js
@@ -1,20 +1,16 @@
-// eslint-disable-next-line no-console
 const originalError = console.error
 
-  // eslint-disable-next-line no-console
-  console.error = (...args) => {
-    // Suppress jest error msg
-    if (
-      args[0].includes('ReactDOM.render is no longer supported in React 18.')
-    ) {
-      return
-    } else if (
-      args[0].includes(
-        'Warning: An update to %s inside a test was not wrapped in act'
-      )
-    ) {
-      return
-    }
-
-    originalError.call(console, ...args)
+console.error = (...args) => {
+  // Suppress jest error msg
+  if (args[0].includes('ReactDOM.render is no longer supported in React 18.')) {
+    return
+  } else if (
+    args[0].includes(
+      'Warning: An update to %s inside a test was not wrapped in act'
+    )
+  ) {
+    return
   }
+
+  originalError.call(console, ...args)
+}

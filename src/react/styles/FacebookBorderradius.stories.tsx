299d5e2a66aa9fd1af46c70ba46a004df816786d
ryota-murakami <dojce1048@gmail.com>
2023-04-18 19:29:19 +0900

    add story

---
+++ src/react/styles/FacebookBorderradius.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from '@storybook/react'
+
+import FacebookBorderRadius from './FacebookBorderRadius'
+
+export default {
+  component: FacebookBorderRadius,
+  title: 'FacebookBorderRadius',
+} as Meta<typeof FacebookBorderRadius>
+
+export const Default = {}

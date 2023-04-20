e3c424ede5ebf2cb7f7c2637b8ea3d01a6402b73
ryota-murakami <dojce1048@gmail.com>
2023-04-20 11:03:16 +0900

    add Spiner stories

---
+++ src/react/elements/Spinner.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Spinner from './Spinner'
+
+const meta: Meta<typeof Spinner> = {
+  component: Spinner,
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Elements/Spinner',
+}
+
+export default meta
+type Story = StoryObj<typeof Spinner>
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => <Spinner />,
+}

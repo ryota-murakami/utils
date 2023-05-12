7806c59963ddc972b46337878566b299df9f84f4
ryota-murakami <dojce1048@gmail.com>
2023-05-12 12:29:24 +0900

    up

---
+++ src/node/git.js
@@ -0,0 +1,17 @@
+const exec = require('child_process')
+
+/**
+ * executes a git subcommand
+ * @param {any} args
+ */
+function git(args) {
+  return new Promise((resolve, reject) => {
+    exec(`git ${args}`, (err, stdout) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(stdout.trim())
+      }
+    })
+  })
+}

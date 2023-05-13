e671e51a4c2cfc84e58ba3e576810cdd8dc81888
ryota-murakami <dojce1048@gmail.com>
2023-05-13 16:38:25 +0900

    fix lint

--- src/react/layouts/Max80remCointainer.tsx
+++ src/react/layouts/Max80remCointainer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 const Max80remCointainer: React.FC<{ children: React.ReactNode }> = ({
   children,
-}) => <div className="max-w-7xl sm:px-6 lg:px-8 mx-auto">{children}</div>
+}) => <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">{children}</div>
 
 export default Max80remCointainer
--- src/react/pages/index.tsx
+++ src/react/pages/index.tsx
@@ -57,7 +57,7 @@ const Index: React.FC = () => {
       <Transition.Root show={sidebarOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="md:hidden fixed inset-0 z-40 flex"
+          className="fixed inset-0 z-40 flex md:hidden"
           onClose={setSidebarOpen}
         >
           <Transition.Child
@@ -80,7 +80,7 @@ const Index: React.FC = () => {
             leaveFrom="translate-x-0"
             leaveTo="-translate-x-full"
           >
-            <div className="relative flex flex-col flex-1 w-full max-w-xs pt-5 pb-4 bg-indigo-700">
+            <div className="relative flex w-full max-w-xs flex-1 flex-col bg-indigo-700 pb-4 pt-5">
               <Transition.Child
                 as={Fragment}
                 enter="ease-in-out duration-300"
@@ -90,29 +90,29 @@ const Index: React.FC = () => {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <div className="absolute top-0 right-0 pt-2 -mr-12">
+                <div className="absolute right-0 top-0 -mr-12 pt-2">
                   <button
                     type="button"
-                    className="focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white flex items-center justify-center w-10 h-10 ml-1 rounded-full"
+                    className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                     onClick={() => setSidebarOpen(false)}
                   >
                     <span className="sr-only">Close sidebar</span>
                     <XMarkIcon
-                      className="w-6 h-6 text-white"
+                      className="h-6 w-6 text-white"
                       aria-hidden="true"
                     />
                   </button>
                 </div>
               </Transition.Child>
-              <div className="flex items-center flex-shrink-0 px-4">
+              <div className="flex flex-shrink-0 items-center px-4">
                 <img
-                  className="w-auto h-8"
+                  className="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
                   alt="Workflow"
                 />
               </div>
-              <div className="flex-1 h-0 mt-5 overflow-y-auto">
-                <nav className="px-2 space-y-1">
+              <div className="mt-5 h-0 flex-1 overflow-y-auto">
+                <nav className="space-y-1 px-2">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
@@ -121,11 +121,11 @@ const Index: React.FC = () => {
                         item.current
                           ? 'bg-indigo-800 text-white'
                           : 'text-indigo-100 hover:bg-indigo-600',
-                        'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                        'group flex items-center rounded-md px-2 py-2 text-base font-medium'
                       )}
                     >
                       <item.icon
-                        className="flex-shrink-0 w-6 h-6 mr-4 text-indigo-300"
+                        className="mr-4 h-6 w-6 flex-shrink-0 text-indigo-300"
                         aria-hidden="true"
                       />
                       {item.name}
@@ -142,19 +142,19 @@ const Index: React.FC = () => {
       </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className="md:flex md:flex-shrink-0 hidden bg-indigo-700">
-        <div className="flex flex-col w-64">
+      <div className="hidden bg-indigo-700 md:flex md:flex-shrink-0">
+        <div className="flex w-64 flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto">
-            <div className="flex items-center flex-shrink-0 px-4">
+          <div className="flex flex-grow flex-col overflow-y-auto pb-4 pt-5">
+            <div className="flex flex-shrink-0 items-center px-4">
               <img
-                className="w-auto h-8"
+                className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
                 alt="Workflow"
               />
             </div>
-            <div className="flex flex-col flex-1 mt-5">
-              <nav className="flex-1 px-2 space-y-1">
+            <div className="mt-5 flex flex-1 flex-col">
+              <nav className="flex-1 space-y-1 px-2">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
@@ -163,11 +163,11 @@ const Index: React.FC = () => {
                       item.current
                         ? 'bg-indigo-800 text-white'
                         : 'text-indigo-100 hover:bg-indigo-600',
-                      'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                      'group flex items-center rounded-md px-2 py-2 text-sm font-medium'
                     )}
                   >
                     <item.icon
-                      className="flex-shrink-0 w-6 h-6 mr-3 text-indigo-300"
+                      className="mr-3 h-6 w-6 flex-shrink-0 text-indigo-300"
                       aria-hidden="true"
                     />
                     {item.name}
@@ -178,36 +178,36 @@ const Index: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col flex-1 w-0 overflow-hidden">
-        <div className="relative z-10 flex flex-shrink-0 h-16 bg-white shadow">
+      <div className="flex w-0 flex-1 flex-col overflow-hidden">
+        <div className="relative z-10 flex h-16 flex-shrink-0 bg-white shadow">
           <button
             type="button"
-            className="focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden px-4 text-gray-500 border-r border-gray-200"
+            className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
             onClick={() => setSidebarOpen(true)}
           >
             <span className="sr-only">Open sidebar</span>
-            <Bars3BottomLeftIcon className="w-6 h-6" aria-hidden="true" />
+            <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
           </button>
-          <div className="flex justify-between flex-1 px-4">
+          <div className="flex flex-1 justify-between px-4">
             <div className="flex flex-1">
               <form
-                className="md:ml-0 flex w-full"
+                className="flex w-full md:ml-0"
                 action="pages/index#"
                 method="GET"
               >
                 <label htmlFor="search-field" className="sr-only">
                   Search
                 </label>
-                <div className="focus-within:text-gray-600 relative w-full text-gray-400">
-                  <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
                     <MagnifyingGlassIcon
-                      className="w-5 h-5"
+                      className="h-5 w-5"
                       aria-hidden="true"
                     />
                   </div>
                   <input
                     id="search-field"
-                    className="focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 border-transparent"
+                    className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
                     placeholder="Search"
                     type="search"
                     name="search"
@@ -215,19 +215,19 @@ const Index: React.FC = () => {
                 </div>
               </form>
             </div>
-            <div className="md:ml-6 flex items-center ml-4">
-              <button className="hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 p-1 text-gray-400 bg-white rounded-full">
+            <div className="ml-4 flex items-center md:ml-6">
+              <button className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span className="sr-only">View notifications</span>
-                <BellIcon className="w-6 h-6" aria-hidden="true" />
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
               </button>
 
               {/* Profile dropdown */}
               <Menu as="div" className="relative ml-3">
                 <div>
-                  <Menu.Button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 flex items-center max-w-xs text-sm bg-white rounded-full">
+                  <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                     <span className="sr-only">Open user menu</span>
                     <img
-                      className="w-8 h-8 rounded-full"
+                      className="h-8 w-8 rounded-full"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       alt=""
                     />
@@ -242,7 +242,7 @@ const Index: React.FC = () => {
                   leaveFrom="transform opacity-100 scale-100"
                   leaveTo="transform opacity-0 scale-95"
                 >
-                  <Menu.Items className="ring-1 ring-black ring-opacity-5 focus:outline-none absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg">
+                  <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     {userNavigation.map((item) => (
                       <Menu.Item key={item.name}>
                         {({ active }) => (
@@ -265,17 +265,17 @@ const Index: React.FC = () => {
           </div>
         </div>
 
-        <main className="focus:outline-none relative flex-1 overflow-y-auto">
+        <main className="relative flex-1 overflow-y-auto focus:outline-none">
           <div className="py-6">
-            <div className="max-w-7xl sm:px-6 md:px-8 px-4 mx-auto">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
               <h1 className="text-2xl font-semibold text-gray-900">
                 Dashboard
               </h1>
             </div>
-            <div className="max-w-7xl sm:px-6 md:px-8 px-4 mx-auto">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
               {/* Replace with your content */}
               <div className="py-4">
-                <div className="h-96 border-4 border-gray-200 border-dashed rounded-lg">
+                <div className="h-96 rounded-lg border-4 border-dashed border-gray-200">
                   <Header />
                 </div>
               </div>

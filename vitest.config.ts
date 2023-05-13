7f5c57e3b6e78053fe05078cb9b028498ba0de6f
ryota-murakami <dojce1048@gmail.com>
2023-05-13 14:38:31 +0900

    fix test

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
     environment: 'jsdom',
     exclude: ['tsc-dist'],
     globals: true,
+    include: [
+      'src/**/*.test.ts',
+      'src/**/*.test.tsx',
+      'src/**/*.test.js',
+      'src/**/*.test.jsx',
+    ],
     setupFiles: ['./setupTests.ts'],
   },
 })

7957a4edb1faff02f05e8378416935e1990e0656
ryota-murakami <dojce1048@gmail.com>
2023-05-13 16:21:00 +0900

    fix lint

--- src/react/main.tsx
+++ src/react/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import App from './App'
 import Pages from './pages'
 
 import './index.css'
-
-import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 

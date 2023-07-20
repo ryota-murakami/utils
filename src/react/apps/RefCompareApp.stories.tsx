2355ddd95ca812bbc753e0d03ef52cb4466dff47
ryota-murakami <dojce1048@gmail.com>
2023-07-20 21:36:51 +0900

    up story

---
+++ src/react/apps/RefCompareApp.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import RefCompareApp from './RefCompereApp'
+
+const meta: Meta<typeof RefCompareApp> = {
+  component: RefCompareApp,
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'RefCompareApp',
+}
+
+export default meta
+type Story = StoryObj<typeof RefCompareApp>
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => <RefCompareApp />,
+}

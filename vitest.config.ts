690205f23e3b41a934fdbd789cd58c96994747db
ryota-murakami <dojce1048@gmail.com>
2023-08-23 18:00:04 +0900

    fix test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 import EnvironmentPlugin from 'vite-plugin-environment'
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
-  plugins: [EnvironmentPlugin([])],
+  //plugins: [EnvironmentPlugin([])],
   test: {
     environment: 'jsdom',
     exclude: ['tsc-dist'],

4933011f4a7abf01cd740fb75332b66b0e20cd6b
ryota-murakami <dojce1048@gmail.com>
2023-11-09 08:20:13 +0900

    up

--- setupTests.ts
+++ setupTests.ts
@@ -7,3 +7,18 @@ beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterEach(() => server.resetHandlers())
 
 afterAll(() => server.close())
+
+//const originalError = console.error;
+//beforeAll(() => {
+//  console.error = (...args) => {
+//    // Supress noisy DOM strcture error messages while "yarn test"
+//    if (args[0].includes("Warning: validateDOMNesting(...)")) {
+//      return;
+//    }
+//    originalError.call(console, ...args);
+//  };
+//});
+//
+//afterAll(() => {
+//  console.error = originalError;
+//});

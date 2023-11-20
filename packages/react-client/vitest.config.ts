d38a7602c0c81cadcc7583e8479ad133dc4e6c1f
ryota-murakami <dojce1048@gmail.com>
2023-11-20 13:23:12 +0900

    fix test

--- packages/react-client/vitest.config.ts
+++ packages/react-client/vitest.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    exclude: ['tsc-dist', 'packages/types'],
     globals: true,
     include: [
-      'packages/**/*.test.ts',
-      'packages/**/*.test.tsx',
-      'packages/**/*.test.js',
-      'packages/**/*.test.jsx',
+      './**/*.test.ts',
+      './**/*.test.tsx',
+      './**/*.test.js',
+      './**/*.test.jsx',
     ],
     setupFiles: ['./setupTests.ts'],
   },

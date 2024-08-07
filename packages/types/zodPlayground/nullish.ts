4453ba63b9b315df5e6d659d5d1c080bed8bc1d4
Ryota Murakami <dojce1048@gmail.com>
2024-08-07 18:45:03 +0900

    featzodPlayground: add validation for age and improve error handling
    - Added a validation rule to ensure age is greater than 10 with a custom error message.
    - Updated the test case to check for age validation.
    - Enhanced error

--- packages/types/zodPlayground/nullish.ts
+++ packages/types/zodPlayground/nullish.ts
@@ -3,8 +3,11 @@ import z from 'zod'
 const schema = z
   .object({
     nameList: z.array(z.string()),
-    age: z.number(),
+    age: z.number().gt(10, {message: 'age should be greater than 10'}),
   })
   .nullish()
 
-console.log(schema.safeParse({}))
+const res = schema.safeParse({age: 9})
+
+console.log(res.error.flatten().fieldErrors)
+
